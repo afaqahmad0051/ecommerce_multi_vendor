00b2dd1227b47f63e732924148290bc50dad1e3c
afaqahmad0051 <afaq.6flicks@gmail.com>
2023-03-04 12:03:20 +0500

    User Last Seen Working

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -43,7 +44,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
+    /* User Active Now */
+     public function userOnline()
+     {
+        return Cache::has('user-online' . $this->id);
+     }
+    
     
     public function year(){
         return $this->belongsTo(Year::class,'year_id','id');
